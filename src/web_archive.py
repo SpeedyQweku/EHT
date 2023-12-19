90b889fddf40c3a5adeae746686b06c541ba6d95
SpeedyQweku <princealex2024@gmail.com>
2023-12-19 01:54:57 +0000

    web_archive

---
+++ src/web_archive.py
@@ -0,0 +1,26 @@
+import requests
+from sys import argv
+
+
+def main(domain):
+    try:
+        url = f"https://web.archive.org/cdx/search/cdx?url={domain}&fl=original&collapse=urlkey"
+        response = requests.get(url)
+        with open(f"{domain}.txt", "+a") as w:
+            for urls in response.text:
+                w.write(urls)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+if __name__ == "__main__":
+    file_name = argv[1]
+    with open(file_name, 'r') as f:
+        for domain in f.readlines():
+            try:
+                main(domain.strip())
+                print(f"[>] {domain} - Successful")
+            except requests.exceptions.ConnectTimeout:
+                print(f"[!] {domain} - Failed, Retry...")
+                main(domain.strip())
+                print(f"[>] {domain} - Successful")
