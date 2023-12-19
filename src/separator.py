2b2d9fc2cd91b1e162c5ff8fc25da7d4067999c3
SpeedyQweku <princealex2024@gmail.com>
2023-12-19 01:54:07 +0000

    separator

---
+++ src/separator.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+with open(argv[1], "r") as rfile:
+    for domains in rfile.readlines():
+        if domains.startswith("*."):
+            with open("wildcard_domain.txt", "a+") as wfile:
+                wfile.write(f"{domains.strip()}\n")
+        else:
+            with open("domain.txt", "a+") as wfile:
+                wfile.write(f"{domains.strip()}\n")
